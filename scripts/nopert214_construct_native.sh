7e76feded33dd1bb1472b07fbe6db15fb5d26108
David Renshaw <dwrenshaw@gmail.com>
2026-07-21 19:04:27 -0400

    Reuse advanced global chart checkpoints

--- scripts/nopert214_construct_native.sh
+++ scripts/nopert214_construct_native.sh
@@ -35,8 +35,8 @@ pack_local 2 local-view-r10-child2.json
 pack_local 3 local-view-r10-child3.json
 
 pack_chart 0 correlated-tight-chart0.json
-pack_chart 1 fundamental-tight-chart1.json
-pack_chart 2 fundamental-tight-chart2.json
+pack_chart 1 correlated-tight-chart1.json
+pack_chart 2 correlated-tight-chart2.json
 
 lake build constructNopert214
 exec .lake/build/bin/constructNopert214 "$artifact_dir"
